b1977f960d7af6105f77eacd9f01ca99d5d786bf
darshandivakarnaik28 <darshunaik28@gmail.com>
2026-09-18 18:42:57 +0530

    cipp

--- INTERVIEW PREPARATION/Prime Number or not/prime.py
+++ INTERVIEW PREPARATION/Prime Number or not/prime.py
@@ -4,3 +4,9 @@ def print_prime(n):
         if n%i==0:
             count+=1
     return count
+n=int(input("Enter a positive number:"))
+count=print_prime(n)
+if count==2:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
