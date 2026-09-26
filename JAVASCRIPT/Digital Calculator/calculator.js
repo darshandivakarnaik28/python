da15e8dac560c76a35e4993a5e9e8fcc4b26c120
darshandivakarnaik28 <darshunaik28@gmail.com>
2026-09-26 19:14:09 +0530

    appear all numeric button to display

--- JAVASCRIPT/Digital Calculator/calculator.js
+++ JAVASCRIPT/Digital Calculator/calculator.js
@@ -3,6 +3,26 @@ const numbers=document.getElementsByClassName('number');
 for (let i=0;i<numbers.length;i++){
     numbers[i].addEventListener("click",function(){
         const num=numbers[i].textContent;
-        display.value=num;
+        display.value+=num;
     });
 }
+const clear=document.getElementById("clear");
+clear.addEventListener("click",function(){
+    display.value="";
+});
+
+const del=document.getElementById("delete");
+del.addEventListener("click",function(){
+    display.value=display.value.slice(0,-1);
+});
+
+const decimal=document.getElementById("decimal");
+decimal.addEventListener("click",function(){
+if(display.value.includes(".")){
+    
+}
+else{
+    display.value+=decimal.textContent;
+}
+});
+
